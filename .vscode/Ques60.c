252c5ce1784ebc6b8dc18dfe40a4d349d1c9f053
gargpriyanshu66 <ishuvivoy53@gmail.com>
2023-05-23 13:01:49 +0530

    hello

--- .vscode/Ques60.c
+++ .vscode/Ques60.c
@@ -11,8 +11,8 @@ typedef struct bank{
 } acc ;
 
 int main(){
-    acc acc1 = {"ishu",1235};
-    acc acc2 = {"parth",1685};
+    acc acc1 = {"ishu",1445};
+    acc acc2 = {"parth",1689};
 
     printf("\ncust1 acc no = %d and  name = %s\n",acc1.accountNo,acc1.name);
     printf("cust2 acc no = %d and name = %s\n",acc2.accountNo,acc2.name);
